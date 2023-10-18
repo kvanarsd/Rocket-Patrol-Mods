fd4653159f22365755e776829c7372f4a869075f
Katrina VanArsdale <kvanarsd@ucsc.edu>
2023-10-17 18:43:17 -0700

    Finished

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,6 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.isFiring = false;
         this.moveSpeed = 2;
+        this.sfxRocket = scene.sound.add("sfx_rocket");
     }
 
     update() {
@@ -16,8 +17,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
 
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();
         }
 
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
@@ -28,5 +30,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false;
             this.y = game.config.height - borderUISize - borderPadding;
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
